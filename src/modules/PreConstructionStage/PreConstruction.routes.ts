cb35b168c0286e4356346ee5def6b1c738b4a792
raghurammunjappakuttam-droid <raghurammunjappakuttam@gmail.com>
2026-04-28 16:25:43 +0530

    added validation in project Id

--- src/modules/PreConstructionStage/PreConstruction.routes.ts
+++ src/modules/PreConstructionStage/PreConstruction.routes.ts
@@ -8,27 +8,18 @@ const router = express.Router();
 
 router.post(
   "/createPreConstruction",
-  // upload.fields([
-  //   { name: "waterSupplyPhotos", maxCount: 3 },
-  //   { name: "toiletPhotos", maxCount: 3 },
-  //   { name: "electricityPhotos", maxCount: 3 },x 
-  //   { name: "labourPhotos", maxCount: 3 },
-  //   { name: "materialPhotos", maxCount: 3 },
-  //   { name: "accessRoadPhotos", maxCount: 3 }
-  // ]),
+  upload.fields([
+    { name: "waterSupplyPhotos", maxCount: 3 },
+    { name: "toiletPhotos", maxCount: 3 },
+    { name: "electricityPhotos", maxCount: 3 },
+    { name: "labourPhotos", maxCount: 3 },
+    { name: "materialPhotos", maxCount: 3 },
+    { name: "accessRoadPhotos", maxCount: 3 }
+  ]),
   validateRequest(createPreConstructionSchema),
   createPreConstruction
 );
 
-router.get("/test", (req, res) => {
-  console.log("req.user",req.user)
-  console.log("working")
-  // res.send("Server working ✅");
-   res.status(201).json({
-      success: true,
-      message: "Created",
-      data: "result"
-    });
-});
+
 
 export default router;
